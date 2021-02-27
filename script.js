eda3f2196fafe2167ad37c938e9026dc14039afa
Mahedi Kamal <mahedikamal44@gmail.com>
2021-02-27 13:18:20 +0600

    Created script.js and added more button.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// ..........popup functions for "My project " section
+function myFunctionGre() {
+    var popup = document.getElementById("myPopupGre");
+    popup.classList.toggle("show");
+}
+function myFunctionPuzzle() {
+    var popup = document.getElementById("myPopupPuzzle");
+    popup.classList.toggle("show");
+}
+function myFunctionSocket() {
+    var popup = document.getElementById("myPopupSocket");
+    popup.classList.toggle("show");
+}
+function myFunctionGword() {
+    var popup = document.getElementById("myPopupGword");
+    popup.classList.toggle("show");
+}
